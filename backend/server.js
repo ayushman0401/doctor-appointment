dd363dcc122d985123d6d170f4a33d6126e624f5
ayushman0401 <ayushmanagarwal789@gmail.com>
2025-01-21 19:14:52 +0530

    initial commit: basic frontend and backend structure

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js';
+//app config
+const app=express();
+const port=process.env.PORT || 4000;
+connectDB();
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+//api endpoint
+app.get('/',(req,res)=>{
+    res.send('api working great')
+});
+app.listen(port,()=>console.log("server started at ",port));
