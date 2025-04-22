163c5346fe6a98b0e7825b9992c74b686a8c8f93
ayushman0401 <ayushmanagarwal789@gmail.com>
2025-04-22 17:13:31 +0530

    admin panel complete

--- admin/src/context/AppContext.jsx
+++ admin/src/context/AppContext.jsx
@@ -3,8 +3,22 @@ import { createContext } from "react";
 export const AppContext=createContext()
 
 const AppContextProvider=(props)=>{
+    const calculateAge=(dob)=>{
+        const ndob=new Date(dob)
+        const today=new Date()
+        let age=today.getFullYear()-ndob.getFullYear()
+        
+        return age;
+    }
+    const months=["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    
+    const slotDateFormat=(date)=>{
+        const dateArray=date.split('_');
+        return dateArray[0]+' '+months[(dateArray[1])-1]+' '+dateArray[2];
+    
+      }  
     const value={
-
+        calculateAge,slotDateFormat
     }
     return (
         <AppContext.Provider value={value}>
